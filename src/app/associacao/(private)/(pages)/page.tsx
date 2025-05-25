aa43cb93e0bdef7607040c07664cb3552c08d7f7
guilhermebittelbrunn <guilherme.bitte@unifebe.edu.br>
2025-05-24 23:35:33 -0300

    chore: trigger deployment

--- src/app/associacao/(private)/(pages)/page.tsx
+++ src/app/associacao/(private)/(pages)/page.tsx
@@ -12,7 +12,7 @@ import RecentActivities from '../(ui-elements)/recentActivities';
 export default function Home() {
     return (
         <div className="grid grid-cols-12 gap-4 md:gap-6">
-            <div className="col-span-12 space-y-6 xl:col-span-10">
+            <div className="col-span-12 space-y-6 xl:col-span-12">
                 <OverviewCards />
             </div>
 
